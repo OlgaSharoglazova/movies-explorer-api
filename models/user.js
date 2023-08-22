4a2e1e0e0dc7aff3928bbbdd409259843aa9d3af
Sharoglazova Olga <scharoglazovao@yandex.ru>
2023-08-22 18:25:52 +0800

    схема пользователя

---
+++ models/user.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+const bcrypt = require('bcrypt');
+const Unauthorized = require('../errors/unauthorized');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    minlength: 2,
+    maxlength: 30,
+  },
+  email: {
+    type: String,
+    validate: {
+      validator: (email) => validator.isEmail(email),
+      message: 'Неправильный формат почты',
+    },
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    select: false,
+  },
+});
+
+userSchema.statics.findUserByCredentials = function (email, password) {
+  return this.findOne({ email }).select('+password')
+    .then((user) => {
+      if (!user) {
+        return Promise.reject(new Unauthorized('Неправильные почта или пароль'));
+      }
+
+      return bcrypt.compare(password, user.password)
+        .then((matched) => {
+          if (!matched) {
+            return Promise.reject(new Unauthorized('Неправильные почта или пароль'));
+          }
+
+          return user;
+        });
+    });
+};
+
+module.exports = mongoose.model('user', userSchema);
