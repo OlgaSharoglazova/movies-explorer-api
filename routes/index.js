d4ecea4d25d294e88776bf1e0f8d4e816748e0dd
Sharoglazova Olga <scharoglazovao@yandex.ru>
2023-08-27 00:02:49 +0800

    поле имя обязательное

--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,7 @@ const NotFound = require('../errors/notFound');
 
 routes.post('/signup', celebrate({
   body: Joi.object().keys({
-    name: Joi.string().min(2).max(30),
+    name: Joi.string().required().min(2).max(30),
     email: Joi.string().required().email(),
     password: Joi.string().required().min(6),
   }),
