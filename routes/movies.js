ef476b825d1dd9d5dad18e86ec3f97bc2ab189ff
Sharoglazova Olga <scharoglazovao@yandex.ru>
2023-10-24 13:59:30 +0800

    исправить роут удаления фильма

--- routes/movies.js
+++ routes/movies.js
@@ -26,7 +26,7 @@ moviesRoutes.post('/', celebrate({
   }),
 }), saveMovie);
 
-moviesRoutes.delete('/movieId', celebrate({
+moviesRoutes.delete('/:movieId', celebrate({
   params: Joi.object().keys({
     movieId: Joi.string().required(),
   }),
